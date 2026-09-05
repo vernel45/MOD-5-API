304cc8587e3d15475656e0e9d7dfdde4556a1620
Sam Verneuille <samvernel268@gmail.com>
2026-09-05 19:36:20 -0400

    updated

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
 // API 1: "https://jsonplaceholder.typicode.com/users"
 // API 2: "https://jsonplaceholder.typicode.com/posts?userId=:id"
-
+const userListEl = document.querySelector(".user-list");
 async function main() {
   const users = await fetch("https://jsonplaceholder.typicode.com/users");
   const usersData = await users.json();
 
-  usersData.map(
-    (user) => 
- const x = `<div class="user-card">
-            <div class="user-card__container">
-              <h3>User's Name</h4>
-                <p><b>Email:</b> email@email.com</p>
-                <p><b>Phone:</b> 0000000000</p>
-                <p><b>Website:</b> <a href="https://website.website" target="_blank">website.website</a></p>
-            </div>
-          </div>`
-)
-    
-};
+  userListEl.innerHTML = usersData.map((user) => userHTML(user)).join("");
+}
 
 main();
+
+function showUserPosts(user) {
+  console.log(user);
+}
+
+function userHTML(user) {
+  return `<div class="user">
+        <div class="user-card" onclick="showUserPosts(${user.id})">
+            <div class="user-card__container">
+              <h3>${user.name}</h4>
+                <p><b>Email:</b> ${user.email}</p>
+                <p><b>Phone:</b> ${user.phone}</p>
+                <p><b>Website:</b> <a href="https://${user.website}" target="_blank">${user.website}</a></p>
+            </div>
+        </div>
+      </div>`;
+}
