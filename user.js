6c3f256fd0ace312c92cf9c3455f8be51ade1f51
Sam Verneuille <samvernel268@gmail.com>
2026-09-06 16:19:35 -0400

    clean up code

--- user.js
+++ user.js
@@ -27,8 +27,8 @@ async function renderPosts(id) {
   console.log(postsData);
 }
 
-async function main() {
-  const id = localStorage.getItem("id");
+async function renderPosts() {
+ 
   const posts = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${id}`);
   const postsData = await posts.json();
   console.log(postsData);
